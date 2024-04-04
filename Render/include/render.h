b97887f6beacde0ba12adcbd092fdddd39d8c675
William <williampascal_official@outlook.com>
2024-04-04 23:18:49 +0800

    Last commit before focus on major development (Var, Array etc.)

--- Render/include/render.h
+++ Render/include/render.h
@@ -5,12 +5,12 @@
 # include <Compound/pen.h>
 
 /** Rendering */
-void compound_pen_render_refresh();
-void compound_pen_render_full();
-void compound_pen_render_full_with(int *buff);
-void compound_pen_render_partial(Selection selc);
-void compound_pen_render_partial_with(Selection selc, int *buff);
-void compound_pen_render_highlight(Selection selc, int method);
-void compound_pen_set_cursor_highlight_method(Pen *pen, int method);
+void pen_render_refresh();
+void pen_render_full();
+void pen_render_full_with(int *buff);
+void pen_render_partial(Selection selc);
+void pen_render_partial_with(Selection selc, int *buff);
+void pen_render_highlight(Selection selc, int method);
+void pen_set_cursor_highlight_method(Pen *pen, int method);
 
 #endif  /* RENDER_H */
