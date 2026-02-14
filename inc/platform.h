736c6d593910ff864ef8fccee08d770e6330152e
William <william_pascal@outlook.com>
2026-02-14 21:39:43 +0800

    -#include <bits/wordsize.h>

--- inc/platform.h
+++ inc/platform.h
@@ -1,17 +1,17 @@
 /*
  * This file is part of Compound library.
  * Copyright (C) 2024-2026  William Lee
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, see
  * <https://www.gnu.org/licenses/>.
@@ -21,8 +21,6 @@
 
 #ifndef COMPOUND_PLATFORM_H
 # define COMPOUND_PLATFORM_H
-
-# include <bits/wordsize.h>
 
 # define __COMPOUND__  1
 
